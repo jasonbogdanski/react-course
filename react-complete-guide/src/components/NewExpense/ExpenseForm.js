e478f44ce85504b2e786c7ddd9de4aff1d9c375b
Jason Bogdanski <jason.bogdanski@headspring.com>
2022-03-24 11:49:55 -0400

    Added new expense button with show form and hide logic.

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -64,8 +64,9 @@ const ExpenseForm = (props) => {
           ></input>
         </div>
       </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+      <div className="new-expense__controls">
+          <button type="button" onClick={props.onCancel}>Cancel</button>
+          <button type="submit">Add Expense</button>
       </div>
     </form>
   );
--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  let content = (
+      <button onClick={startEditingHandler}>Add New Expense</button>
+  );
+
+  if (isEditing) {
+    content = (
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        onCancel={stopEditingHandler}
+      />
+    );
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {content}
     </div>
   );
 };
